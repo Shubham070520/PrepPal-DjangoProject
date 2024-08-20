645ece06244e74f99352f2a3a068495fd6a05797
Shubham Rane <shubhamrane016@gmail.com>
2024-08-21 00:46:53 +0530

    Created Home page ,Views,Urls

---
+++ website/App/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from App import views
+
+urlpatterns = [
+    path('',views.welcome),
+    path('candidateRegistration',views.candidateRegForm),
+    path('submit',views.candidateRegistration),
+    path('login',views.loginView),
+    path('home',views.candidateHome),
+    path('testPaper',views.testPaper),
+    path('calc-Result',views.calcTestRes),
+    path('test-History',views.testResHistory),
+    path('result',views.showTestRes),
+    path('logout',views.logoutView),
+
+]
--- website/App/views.py
+++ website/App/views.py
@@ -1,10 +1,44 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from App.models import Candidate,Questions,Result
 from django.contrib.auth.models import User
 from django.contrib import messages #to add message
 from django.contrib.auth import authenticate,login,logout
 from django.db.models import Q  #to write min and max values
+
+
+def welcome(request):
+    pass
+
+def candidateRegForm(request):
+    pass
+
+def candidateRegistration(request):
+    pass
+
+def loginView(request):
+    pass
+
+def candidateHome(request):
+    pass
+
+def testPaper(request):
+    pass
+
+def calcTestRes(request):
+    pass
+
+def testResHistory(request):
+    pass
+
+def showTestRes(request):
+    pass
+
+def logoutView(request):
+    pass
+
+
+
 
 
 
